9a4cf19518a870f73df151b0e246c3bbc31528a6
slawamarlow <86040371+slawamarlow@users.noreply.github.com>
2024-03-13 18:49:53 +0500

    Add files via upload

---
+++ src/app/pages/LoginPage/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import {LoginForms} from "@/widgets/FromLogin/ui"
+
+export default function LoginForm() {
+  return (
+    <>
+    <LoginForms/>
+    </>
+  );
+}
---
+++ src/app/pages/RegistorPage/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import {FormRegistr} from "@/widgets/FormRegistr/ui/index"
+
+export default function Register() {
+  return (
+    <>
+    <FormRegistr/>
+  
+    </>
+  );
+}
